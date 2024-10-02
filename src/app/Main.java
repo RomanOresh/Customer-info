7d71e206e645e70e41954bc10a3107dfd069f31b
Roman <romanoreshchenko@gmail.com>
2024-10-02 14:28:51 +0200

    Added Hillel code

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,22 @@
 package app;
 
 public class Main {
+    public static void main(String[] args) {
+        Customer customer = getCustomer(getData());
+        String output = "Customer: ".getName() +
+                ", phone " + customer.getPhone();
+        getOutput(output);
+    }
+
+    public static String[] getData() {
+        return new String[]{/* пропишіть значення для об'єкту Customer*/};
+    }
+
+    public static Customer getCustomer(String[] data) {
+        return new Customer(data[0], data[1]);
+    }
+
+    public static void getOutput(String output) {
+        System.out.println(output);
+    }
 }
